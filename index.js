ccb2f55e43d65e144e59d1bd0f14d419d0daa2c4
Bhavisha262 <138797566+Bhavisha262@users.noreply.github.com>
2024-09-26 09:31:42 +0530

    backl-main

--- index.js
+++ index.js
@@ -37,9 +37,133 @@ const ContactSchema = new mongoose.Schema({
 }); 
 const Contact = mongoose.model("contact-us", ContactSchema);
 
+const NewAccountSchema = new mongoose.Schema({
+    name: {
+    type: String,
+    require: true,
+    },
+    email: {
+    type: String,
+    require: true,
+    },
+    number: {
+    type: Number,
+    require: true,
+    },
+    password: {
+    type: String,
+    require: true,
+    },
+    
+    cart:[
+    {
+    categoryid:{
+    type:Number
+    },
+    productid:{
+    type:Number
+    },
+    img:{
+    type:String
+    },
+    price:{
+    type:String
+    },
+    name:{
+    type:String
+    },
+    quantity:{
+    type:Number,
+    default:1
+    }
+    }
+    ],
+    wish:[
+    {
+    categoryid:{
+    type:Number
+    },
+    productid:{
+    type:Number
+    },
+    img:{
+    type:String
+    },
+    price:{
+    type:String
+    },
+    name:{
+    type:String
+    }
+    }
+    ],
+    shippingInfo: [
+    {
+    name:{
+    type:String
+    },
+    number:{
+    type:Number
+    },
+    email:{
+    type:String
+    },
+    address: {
+    type:String
+    }, 
+    landmark: {
+    type:String
+    } ,
+    state: {
+    type:String
+    },
+    city: {
+    type:String 
+    },
+    
+    pincode: {
+    type:Number
+    } 
+    }
+    ],
+    order: [
+    {
+    orderDate:{ 
+    type: String
+    },
+    categoryid: {
+    type: Number,
+    required:true,
+    },
+    productid:
+    {
+    type: Number,
+    required:true,
+    },
+    img:
+    {
+    type: String,
+    },
+    name:
+    {
+    type: String,
+    },
+    price:
+    {
+    type:String,
+    },
+    quantity:
+    {
+    type: Number,
+    default: 1,
+    }
+    },
+    ],
+});
+const NewAccount = mongoose.model("new-account",  NewAccountSchema);
 
 app.get('/', (req, res) => {
-res.send('Hello fcfcWorld!')
+res.send('Hello Backend Is Live!')
 })
 
 app.listen(3025, () => {
