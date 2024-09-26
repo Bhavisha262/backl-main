9c8539ff9501863052f4755cfbe886f3bea7fbdf
Bhavisha262 <138797566+Bhavisha262@users.noreply.github.com>
2024-09-26 09:13:10 +0530

    backl-main

---
+++ index.js
@@ -0,0 +1,15 @@
+//index.js
+const express = require('express')
+const app = express()
+
+
+app.get('/', (req, res) => {
+res.send('Hello fcfcWorld!')
+})
+
+app.listen(3025, () => {
+console.log("server connected")
+})
+
+//Run the app with the following command
+//node app.js
