194335064f754478461317453b095e5c7ebd6ff9
Bhavisha262 <138797566+Bhavisha262@users.noreply.github.com>
2024-09-26 09:26:32 +0530

    backl-main

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
+const { type } = require('os');
 const app = express()
 
 mongoose
@@ -16,6 +17,25 @@ app.use(cors());
 // app.use(express.static(path.join(__dirname, 'Assets')));
 app.use(bodyParser.json());
 
+const ContactSchema = new mongoose.Schema({
+    name: {
+    type: String,
+    require: true,
+    },
+    email: {
+    type: String,
+    require: true,
+    },
+    number: {
+    type: Number,
+    require: true,
+    },
+    message: {
+    type: String,
+    require: true,
+    },
+}); 
+const Contact = mongoose.model("contact-us", ContactSchema);
 
 
 app.get('/', (req, res) => {
